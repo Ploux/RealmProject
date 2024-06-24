d865eb7e8fcf35b97ec4242b447f893d2865d8af
Ploux <peter@humo.golf>
2024-06-24 10:43:07 -0500

    Initial Commit

---
+++ RealmProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RealmProject
+//
+//  Created by Peter Loux on 6/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RealmProject/RealmProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmProjectApp.swift
+//  RealmProject
+//
+//  Created by Peter Loux on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct RealmProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
